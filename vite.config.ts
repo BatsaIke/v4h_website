7e413ac30ffdc8fc8bf1e8aa6533d2f89c33f59a
batsa-ike <batsa2010isaac@gmail.com>
2024-12-07 23:40:02 +0000

    build success

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,45 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import type { UserConfig } from "vitest/config";
+
+// const test = {
+//   globals: true,
+//   environment: "jsdom",
+//   setupFiles: ["src/__tests__/setupTests.ts"],
+//   threads: false,
+//   watch: false,
+// } as UserConfig["test"];
+
+// const isProd = process.env.NODE_ENV === "production";
+// // Determine if HTTPS is being used
+// const isHttps = process.env.USE_HTTPS === "true";
+// const hmrProtocol = isHttps ? "wss" : "ws";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: parseInt("8000", 10),
+//     https: isHttps, // Enable HTTPS if needed
+//     hmr: {
+//       protocol: hmrProtocol,
+//       host: "diniubire.ro",
+//       port: 24678,
+//     },
+//   },
+//   build: {
+//     minify: isProd,
+//     sourcemap: !isProd, // Enable source maps in development
+//   },
+//   test,
+//   css: {
+//     preprocessorOptions: {
+//       scss: {
+//         additionalData: `@import "./src/client/styles/mixins.scss";\n`,
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import type { UserConfig } from "vitest/config";
@@ -11,24 +53,21 @@ const test = {
 } as UserConfig["test"];
 
 const isProd = process.env.NODE_ENV === "production";
-// Determine if HTTPS is being used
-const isHttps = process.env.USE_HTTPS === "true";
-const hmrProtocol = isHttps ? "wss" : "ws";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: parseInt("8000", 10),
-    https: isHttps, // Enable HTTPS if needed
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3000, // Use Render's PORT or default to 3000
+    strictPort: true, // Ensure the server fails if the port is unavailable
     hmr: {
-      protocol: hmrProtocol,
-      host: "diniubire.ro",
-      port: 24678,
+      protocol: "ws", // Use WebSocket for HMR
+      host: "localhost", // Dynamic for local development
     },
   },
   build: {
     minify: isProd,
     sourcemap: !isProd, // Enable source maps in development
+    chunkSizeWarningLimit: 500, // Prevent warnings about large chunks
   },
   test,
   css: {
